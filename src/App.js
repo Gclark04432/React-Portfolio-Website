7fdeba03be925275f843647f25825f5b60f92b54
Gary Clark <gclark04432@live.co.uk>
2020-03-25 13:30:02 +0000

    setup grid structure on main page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-          Learn React
+          <p className="nav">NavBar Here</p>
+          <p className="about-me">About Me Here</p>
+          <p className="ruby">Ruby Here</p>
+          <p className="javascript">JS Here</p>
+          <p className="java">Java Here</p>
     </div>
   );
 }
