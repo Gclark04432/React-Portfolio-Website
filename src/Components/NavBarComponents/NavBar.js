7787a96a872a0f9c1c59b2d036a534e4f55fa2ac
Gary Clark <gclark04432@live.co.uk>
2020-04-22 13:23:19 +0100

    change navbar css and add name and job title;

--- src/Components/NavBarComponents/NavBar.js
+++ src/Components/NavBarComponents/NavBar.js
@@ -7,12 +7,15 @@ import {
 
 function NavBar() {
   return(
+    <section className="navbar">
+      <p className="title"><span className="my-name">Gary Clark</span> <span className="my-title">| Software Developer</span></p>
       <ul className="navbar-list">
         <Link to="/" className="navbar-list-item active">Home Page</Link>
         <Link to="/about" className="navbar-list-item">About Me</Link>
         <Link to="/projects" className="navbar-list-item">Projects</Link>
         <Link to="contact" className="navbar-list-item">Contact Info</Link>
       </ul>
+    </section>
   )
 
 }
