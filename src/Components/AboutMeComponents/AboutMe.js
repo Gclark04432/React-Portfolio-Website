ee150aafadd91cfcddfe3c34c39af410a1916583
Gary Clark <gclark04432@live.co.uk>
2020-04-23 14:18:39 +0100

    add grid layout to about me page

--- src/Components/AboutMeComponents/AboutMe.js
+++ src/Components/AboutMeComponents/AboutMe.js
@@ -4,9 +4,16 @@ import './AboutMe.css';
 function AboutMe() {
   return(
     <main className="about-me-main">
-    Soon to graduate from the Professional Software Development course at CodeClan and with a BSc in Computer Science from previous Education, I am looking to take the first step in my Software Development career.<br/><br/>
-    While my previous career path was very fruitful and rewarding, I am looking to use my skills in coding, alongside my transferrable skills, to work in an industry that has always been a personal and professional interest of mine, where I can develop to my potential and collaborate closely with my colleagues .<br/><br/>
-    I take pride in my work, regardless of the task, and have a work-ethic to match my drive for development, both in my own abilities and in projects, which I believe will be well suited to a career in tech.
+      <section className="about-me-text">
+        <span className="my-name">Hi, I'm Gary!</span><br/>
+        A Junior Software Developer who loves tackling complex problems in all walks of life and using logic to make the world a slightly brighter place! For some, like me, that is just making things more efficient, building cool apps or having a game to play to take your mind off things. For others it's tracking behaviours and bettering yourself through lifestyle changes, job changes...etc. I've worked on projects covering a range of scenarios and briefs and am now actively seeking full-time employment in the industry, read on for more!
+      </section>
+      <section className="about-unknown">
+        <span className="hobbies-header">Unknown</span>
+      </section>
+      <section className="hobbies-text">
+        <span className="hobbies-header">Hobbies</span>
+      </section>
     </main>
   )
 }
