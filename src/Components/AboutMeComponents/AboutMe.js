c597e374e09dfdbfed07019b6cce45d2db81bf81
Gary Clark <gclark04432@live.co.uk>
2020-05-14 16:10:00 +0100

    amend colour scheme to cool blue

--- src/Components/AboutMeComponents/AboutMe.js
+++ src/Components/AboutMeComponents/AboutMe.js
@@ -5,7 +5,7 @@ function AboutMe() {
   return(
     <main className="about-me-main">
       <section className="about-me-text">
-        <span className="my-name">Hi, I'm Gary!</span><br/>
+        <span className="my-name">Hi, I'm Gary!</span><br/><br/>
         A Graduate Software Developer who loves tackling complex problems in all walks of life and using logic to make the world a slightly brighter place! For some, like me, that is just making things more efficient, building cool apps or having a game to play to take your mind off things. For others it's tracking behaviours and bettering yourself through lifestyle changes, job changes...etc. I've worked on projects covering a range of scenarios and briefs and am now actively seeking full-time employment in the industry, read on for more!
       </section>
       <section className="about-unknown">
